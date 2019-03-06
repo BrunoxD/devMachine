a5a617f9fab1c56b3a2f0388c1901b54dfe7f79e
BrunoxD <bruno.mendes.costa@usp.br>
2019-03-06 08:33:32 -0300

    Scripts adjustments

--- scripts/Install.sh
+++ scripts/Install.sh
@@ -9,13 +9,17 @@ apt-get update \
     && apt-get install -y $buildDeps --no-install-recommends \
     && cat /tmp/library-dependencies.txt | egrep "^[^#].*$" | xargs apt-get install -y \
     && CFLAGS="-Os -g0 -Wl,--strip-all -I/usr/include:/usr/local/include -L/usr/lib:/usr/local/lib" \
-        pip3 install \
-        --no-cache-dir \
-        --compile \
-        --global-option=build_ext \
-        --global-option="-j 4" \
-        --upgrade \
-        -r /tmp/requirements.txt \
+    && 
+    for lib in `cat /tmp/requirements.txt | egrep "^[^#].*$"`
+		do
+			pip3 install \
+			    --no-cache-dir \
+			    --compile \
+			    --global-option=build_ext \
+			    --global-option="-j 4" \
+			    --upgrade \
+				$lib 
+		done \
     && rm -r \
         /tmp/requirements.txt \
         /tmp/library-dependencies.txt
