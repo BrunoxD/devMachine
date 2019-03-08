15f2471abd4a250ec4c3742468999babf7d84e64
BrunoxD <bruno.mendes.costa@usp.br>
2019-03-07 22:27:48 -0300

    Finish install script.

--- scripts/Install.sh
+++ scripts/Install.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 # Execute with root privileges
+# Install pip3 and upgrade it before this.
 
 cp -r ./library-dependencies.txt /tmp/library-dependencies.txt
-cp -r ./requirements.txt /tmp/requirements.txt
-buildDeps='build-essential gcc gfortran python3.6-dev'
+buildDeps='build-essential gcc gfortran python3-dev'
 
 apt-get update \
     && apt-get install -y $buildDeps --no-install-recommends \
     && cat /tmp/library-dependencies.txt | egrep "^[^#].*$" | xargs apt-get install -y \
-    && sudo -H python3 -m pip install --upgrade pip \
-    && sudo -H python3 -m pip install --upgrade jupyter \
-    && CFLAGS="-Os -g0 -Wl,--strip-all -I/usr/include:/usr/local/include -L/usr/lib:/usr/local/lib" \
     && 
-    for lib in `cat /tmp/requirements.txt | egrep "^[^#].*$"`
+    for lib in `cat ./requirements.txt | egrep "^[^#].*$"`
 		do
-			pip3 install \
+			echo $lib > /tmp/requirements.txt \
+			&& cat /tmp/requirements.txt \
+			&& CFLAGS="-Os -g0 -Wl,--strip-all -I/usr/include:/usr/local/include -L/usr/lib:/usr/local/lib" /usr/local/bin/pip install \
 			    --no-cache-dir \
 			    --compile \
 			    --global-option=build_ext \
 			    --global-option="-j 4" \
 			    --upgrade \
-				$lib 
+				-r /tmp/requirements.txt
 		done \
-	&& sudo -H python3 -m pip install --upgrade tensorflow \
     && rm -r \
         /tmp/requirements.txt \
         /tmp/library-dependencies.txt
